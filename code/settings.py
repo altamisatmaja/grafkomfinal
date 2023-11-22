96da1e0c3c7b20fedcce18f5eec4221c7d00b08d
altamisatmaja <altamisfattah16@gmail.com>
2023-11-22 21:26:28 +0700

    setup all

---
+++ code/settings.py
@@ -0,0 +1,19 @@
+level_map = [
+'                                                                                                         X           ',
+'       X                                                                                                 X           ',
+'  X                    X                                                                              X  X     X     ',
+'       X                                                          XXXX                      X     X   X XX           ',
+'             XXX                   X XX     X                 X                         X             X  XXX         ',
+'   X                                                                                                  XX X        X  ',
+'                                   XXX                 XXXX                 XXXX          X           X  X           ',
+'      X                                                   X                                           X XX      X    ',
+'                              XXXX                      XXX                                 XXXX      X  X           ',
+'    X                      X                                                       XXXXX        XXXX  XX X  X        ',
+'P                                                 XXXX                                 X     XXXX     X              ',
+'XXXX                 XXXXX                      XXXX  XX                               XXXXXXX        XXXX   ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
+
+# /Users/altamis/Documents/Semester 3/GrafikaKomputer/project/2 - Level/grafkom/code
